db0d363b2760d767b278833f34e8ff794f2909b7
Błażej Kasperski <kasperski.blazej@gmail.com>
2019-12-15 12:06:29 +0100

    Updates

---
+++ precache-manifest.c166092922bb556edba34315fa096738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e6c7befb3b410b51abc893a6c064bd",
+    "url": "/waluty/index.html"
+  },
+  {
+    "revision": "7212744a3d68c1b33b76",
+    "url": "/waluty/static/css/main.72d2d581.chunk.css"
+  },
+  {
+    "revision": "4fef7defafc142cdba09",
+    "url": "/waluty/static/js/2.5a96c515.chunk.js"
+  },
+  {
+    "revision": "7212744a3d68c1b33b76",
+    "url": "/waluty/static/js/main.26883945.chunk.js"
+  },
+  {
+    "revision": "132205b6c36cc75ce53a",
+    "url": "/waluty/static/js/runtime-main.819ab67e.js"
+  }
+]);
